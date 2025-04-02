50f3d0c45cded55d7d880135ffdeb94566f77264
shrek <bigshrek@gmail.com>
2025-04-02 12:10:36 +0545

    Student, Course and Enrollment

--- student/admin.py
+++ student/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from student.models import Student
+from student.models import Course
+from student.models import Enrollment
 # Register your models here.
 
 admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Enrollment)
